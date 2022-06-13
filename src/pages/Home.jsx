0f1f1f6b9276289454d46d6e0ee2465ee5e8b87a
Arafat <arafat97alim@gmail.com>
2022-06-14 01:00:44 +0530

    boilerplate of home page added.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="header__left">
+          {/* link  Router*/}
+          {/* link Router */}
+        </div>
+        <div className="header__right">
+          {/* link */}
+          {/* link */}
+          {/* icon Material UI */}
+          {/* Avatar Material UI */}
+        </div>
+      </div>
+      <div className="home__body"></div>
+    </div>
+  );
+};
+
+export default Home;
