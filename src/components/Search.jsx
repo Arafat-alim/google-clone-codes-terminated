f829c27d11eec2bdc48f03b405da6be065e8e8f7
Arafat <arafat97alim@gmail.com>
2022-06-15 14:31:57 +0530

    google search bar butotn functionality is created.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
 
-const Search = () => {
+const Search = ({ hidebuttons = false }) => {
+  //creating a state
+  const [input, setInput] = useState("");
+  const history = useNavigate();
+
+  const search = (e) => {
+    e.preventDefault();
+    console.log("You have hit the search button >>>", input);
+
+    history("/search");
+    // history.push("/search");
+  };
+
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputicon" />
-        <input />
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
         <MicIcon />
       </div>
-      <div className="search_buttons">
-        <Button variant="outlined">Google Search</Button>
-        <Button variant="outlined">I'm Feeling Lucky</Button>
-      </div>
-    </div>
+      {!hidebuttons ? (
+        <div className="search_buttons">
+          <Button type="submit" onClick={search} variant="outlined">
+            Google Search
+          </Button>
+          <Button onClick={search} variant="outlined">
+            I'm Feeling Lucky
+          </Button>
+        </div>
+      ) : (
+        <div className="search_buttons ">
+          <Button
+            className="search__buttonshidden"
+            type="submit"
+            onClick={search}
+            variant="outlined"
+          >
+            Google Search
+          </Button>
+          <Button
+            className="search__buttonshidden"
+            onClick={search}
+            variant="outlined"
+          >
+            I'm Feeling Lucky
+          </Button>
+        </div>
+      )}
+    </form>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
           alt="google-logo"
         />
         <div className="home__inputContainer">
-          <Search />
+          <Search hidebuttons />
         </div>
       </div>
     </div>
