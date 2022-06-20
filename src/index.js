44693d5489af487927dbfe3ecaf19f92edef4d0e
Arafat <arafat97alim@gmail.com>
2022-06-20 23:38:53 +0530

    not gettting the desire output.

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import reducer, { initialState } from "./reducer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <App />
-    </StateProvider>
+    <StateProvider
+      initialState={initialState}
+      reducer={reducer}
+    ></StateProvider>
+    <App />
   </React.StrictMode>
 );
 
